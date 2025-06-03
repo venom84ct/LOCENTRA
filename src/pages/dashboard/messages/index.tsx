2e9b6b7e5f68a197d070b56e530905f72674be31
venom84ct <diguardo84@gmail.com>
2025-06-03 12:38:22 +0800

    Update index.tsx

--- src/pages/dashboard/messages/index.tsx
+++ src/pages/dashboard/messages/index.tsx
@@ -1,10 +1,13 @@
-
-// src/pages/dashboard/messages/index.tsx
 import React from "react";
+import DashboardLayout from "@/components/layout/DashboardLayout";
 import SimpleMessagingSystem from "@/components/messaging/SimpleMessagingSystem";
 
 const MessagesPage = () => {
-  return <SimpleMessagingSystem />;
+  return (
+    <DashboardLayout userType="resident">
+      <SimpleMessagingSystem />
+    </DashboardLayout>
+  );
 };
 
 export default MessagesPage;
