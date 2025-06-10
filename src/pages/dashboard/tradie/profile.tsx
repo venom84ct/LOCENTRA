93004c1c77273e3c229d439069a08569fa87fd0a
venom84ct <diguardo84@gmail.com>
2025-06-10 10:30:24 +0800

    Update profile.tsx

--- src/pages/dashboard/tradie/profile.tsx
+++ src/pages/dashboard/tradie/profile.tsx
@@ -1,4 +1,4 @@
-""import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { supabase } from "@/lib/supabaseClient";
 import DashboardLayout from "@/components/layout/DashboardLayout";
 import {
